242b8eae0114ceea27ba3342e400a64330d5d9c0
ascii <wng.yongbin@gmail.com>
2015-07-06 15:36:20 +0800

    init swift project

---
+++ Ubuy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ubuy
+//
+//  Created by ascii on 15/7/6.
+//  Copyright © 2015年 Friday. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
